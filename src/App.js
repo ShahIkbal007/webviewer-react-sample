3fb35cd9ffbcb27ca0dd9d10eb6e6eb707fbedb5
Shah Ikbal Ahmed <76581633+shahikbalahmed@users.noreply.github.com>
2022-10-26 18:02:07 +0530

    Added texarea and style

--- src/App.js
+++ src/App.js
@@ -83,6 +83,7 @@ const App = () => {
                   canvas.toBlob((blob) => {
                     console.log(blob);
                   });
+                  
 
                   console.log(canvas.toDataURL(), "canvas.toDataUrl");
                 },
--- src/QuestionData/index.js
+++ src/QuestionData/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./QuestionData.css";
 
 function QuestionData() {
-  return <div className="questionContainer">Question Data</div>;
+  return (
+    <div className="questionContainer">
+      <textarea
+        id="review"
+        name="review"
+        className="questionContainer__review"
+        placeholder="You can paste here and view your text..."
+      ></textarea>
+    </div>
+  );
 }
 
 export default QuestionData;
